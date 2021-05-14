fc9eab4016f2e2b117566766d790859755e8ab24
Huseyin Koclar <75777777+huseyinkoclar@users.noreply.github.com>
2021-05-14 18:05:58 +0300

    Add files via upload

--- Bin2XML/Bin2XML.c
+++ Bin2XML/Bin2XML.c
@@ -21,57 +21,61 @@ struct record
     unsigned int weight;
 };
 
-struct header
+struct tags
 {
-    char name[64];      //utf16
+    char name[32];      //utf16
     char surname[32];   //utf8
-    char gender;
+    char gender[32];
     char email[32];
-    char phone_number[16];
+    char phone_number[32];
     char address[32];
-    char level_of_education[8];
-    unsigned int income_level;  // given little-endian
-    unsigned int expenditure;   // given big-endian
-    char currency_unit[16];
+    char level_of_education[32];
+    char income_level[32];  // given little-endian
+    char expenditure[32];   // given big-endian
+    char currency_unit[32];
     char currentMood[32];
-    float height;
-    unsigned int weight;
+    char height[32];
+    char weight[32];
 };
 
-int num = 51;
+int num = 50;
 
 int main();
 
 struct record *readFile(char path[]);
 
+struct tags *readFile1(char path[]);
+
 int main(int argc, char const argv[]) {
-    printf("hello assignment");
+    printf("succesfull");
 
     FILE *fp = fopen("Bin2XML.xml", "w");
     struct record *records = readFile("./records.dat");
+    struct tags *tag = readFile1("./records.dat");
     fprintf(fp, "%s","<records>\n");
     for(int i = 1; i < 51; i++){
-        fprintf(fp, "\trow id = %d\n", (i));
-        fprintf(fp, "\t\t<%s>%s</%s>\n\t\t<%s>%s</%s>\n\t\t<%c>%c</%c>\n\t\t<%s>%s</%s>\n"
-        "\t\t<%s>%d</%s>\n\t\t<%s>%s</%s>\n\t\t<%s>%s</%s>\n\t\t<%s>%d</%s>\n\t\t<%s>%d</%s>\n"
-        "\t\t<%s>%s</%s>\n\t\t<%s>%s</%s>\n\t\t<%s>%d</%s>\n\t\t<%s>%d</%s>\n",
-        records[0].name, records[i].name, records[0].name,
-        records[0].surname, records[i].surname, records[0].surname,
-        records[0].gender, records[i].gender, records[0].gender,
-        records[0].email, records[i].email, records[0].email,
-        records[0].phone_number, records[i].phone_number, records[0].phone_number,
-        records[0].address, records[i].address, records[0].address,
-        records[0].level_of_education, records[i].level_of_education, records[0].level_of_education,
-        records[0].income_level, records[i].income_level, records[0].income_level,
-        records[0].expenditure, records[i].expenditure, records[0].expenditure,
-        records[0].currency_unit, records[i].currency_unit, records[0].currency_unit,
-        records[0].currentMood, records[i].currentMood, records[0].currentMood,
-        records[0].height, records[i].height, records[0].height,
-        records[0].weight, records[i].weight, records[0].weight
+        fprintf(fp, "\t<row id = %c%d%c>\n", 34,(i),34);
+        fprintf(fp, "\t\t<%s>%s</%s>\n\t\t<%s>%s</%s>\n\t\t<%s>%c</%s>\n\t\t<%s>%s</%s>\n"
+        "\t\t<%s>%s</%s>\n\t\t<%s>%s</%s>\n\t\t<%s>%s</%s>\n\t\t<%s>%u</%s>\n\t\t<%s>%u</%s>\n"
+        "\t\t<%s>%s</%s>\n\t\t<%s>%s</%s>\n\t\t<%s>%f</%s>\n\t\t<%s>%u</%s>\n",
+        tag->name, records[i].name, tag->name,
+        tag->surname, records[i].surname, tag->surname,
+        tag->gender, records[i].gender, tag->gender,
+        tag->email, records[i].email, tag->email,
+        tag->phone_number, records[i].phone_number, tag->phone_number,
+        tag->address, records[i].address, tag->address,
+        tag->level_of_education, records[i].level_of_education, tag->level_of_education,
+        tag->income_level, records[i].income_level, tag->income_level,
+        tag->expenditure, records[i].expenditure, tag->expenditure,
+        tag->currency_unit, records[i].currency_unit, tag->currency_unit,
+        tag->currentMood, records[i].currentMood, tag->currentMood,
+        tag->height, records[i].height, tag->height,
+        tag->weight, records[i].weight, tag->weight
         );
+        fprintf(fp, "\t</row>\n");
     }
-    fprintf(fp, "%s", "records");
-    printf("sasasa");
+    fprintf(fp, "%s", "</records>");
+    printf("!!!");
     return 0;    
 }
 
@@ -87,10 +91,42 @@ struct record *readFile(char path[])
         return recs;
     }
     struct record rec;
+
     for(int i = 0; i < 51; i++){
         fread(&rec, sizeof(struct record), 1, binFile);
+        if(i != 0){
         recs[i] = rec;
+        }
     }
     fclose(binFile);
     return recs;
+}
+
+struct tags *readFile1(char path[])
+{
+    struct tags *tag = calloc(101, sizeof(struct tags));   
+    FILE *binFile;
+    binFile = fopen(path, "rb");
+    if(binFile == NULL)
+    {
+        printf("File not found exceptioné Cannot find source file.");
+        exit(1);
+        return tag;
+    }
+    fread(tag, sizeof(struct tags), 1, binFile);
+    fclose(binFile);
+    strcpy(tag->gender, "gender");
+    strcpy(tag->email, "email");
+    strcpy(tag->surname, "surname");
+    strcpy(tag->phone_number, "phone_number");
+    strcpy(tag->address, "address");
+    strcpy(tag->level_of_education, "level_of_education");
+    strcpy(tag->income_level, "income_level");
+    strcpy(tag->expenditure, "expenditure");
+    strcpy(tag->currency_unit, "currency_unit");
+    strcpy(tag->currentMood, "currentMood");
+    strcpy(tag->height, "height");
+    strcpy(tag->weight, "weight");
+
+    return tag;
 }
